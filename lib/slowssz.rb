5676a504738241418784df94a2aabe154d2d8441
pinglamb <pinglambs@gmail.com>
2020-12-18 18:34:32 +0800

    ✨ bool, bitvector and bitlist

--- lib/slowssz.rb
+++ lib/slowssz.rb
@@ -1,8 +1,72 @@
 # frozen_string_literal: true
 
-require_relative "slowssz/version"
+require_relative 'slowssz/version'
 
 module Slowssz
-  class Error < StandardError; end
-  # Your code goes here...
+  class ListTooBig < StandardError; end
+
+  class BitVector
+    attr_reader :value
+
+    def <<(bit)
+      @value << bit
+    end
+
+    def size
+      @value.size
+    end
+
+    private
+
+    def initialize(value)
+      @value = value
+    end
+  end
+
+  class BitList
+    attr_reader :value, :capacity
+
+    def <<(bit)
+      raise ListTooBig if @value.size > @capacity
+
+      @value << bit
+    end
+
+    private
+
+    def initialize(value, capacity)
+      @capacity = capacity
+      raise ListTooBig if value.size > @capacity
+
+      @value = value
+    end
+  end
+
+  class Marshal
+    class << self
+      def dump(obj)
+        if obj.is_a?(BitVector)
+          dump_bit_vector(obj)
+        elsif obj.is_a?(BitList)
+          dump_bit_list(obj)
+        elsif obj.is_a?(TrueClass) || obj.is_a?(FalseClass)
+          dump_bool(obj)
+        end
+      end
+
+      private
+
+      def dump_bool(bool)
+        [bool ? '1' : '0'].pack('b')
+      end
+
+      def dump_bit_vector(bit_vector)
+        [bit_vector.value.inject('') { |str, bool| str + (bool ? '1' : '0') }].pack('b*')
+      end
+
+      def dump_bit_list(bit_list)
+        [bit_list.value.inject('') { |str, bool| str + (bool ? '1' : '0') }.concat('1')].pack('b*')
+      end
+    end
+  end
 end
--- spec/slowssz_spec.rb
+++ spec/slowssz_spec.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe Slowssz do
+  it 'bool F' do
+    expect(Slowssz::Marshal.dump(false)).to eq(['00'].pack('H*'))
+  end
+
+  it 'bool T' do
+    expect(Slowssz::Marshal.dump(true)).to eq(['01'].pack('H*'))
+  end
+
+  it 'bitvector TTFTFTFF' do
+    expect(Slowssz::Marshal.dump(Slowssz::BitVector.new([true, true, false, true, false, true, false, false]))).to eq(
+      ['2b'].pack('H*')
+    )
+  end
+
+  it 'bitlist TTFTFTFF' do
+    expect(Slowssz::Marshal.dump(Slowssz::BitList.new([true, true, false, true, false, true, false, false], 8))).to eq(
+      ['2b01'].pack('H*')
+    )
+  end
 end
